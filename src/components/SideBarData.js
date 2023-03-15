57d698eab4c214b2e57a8c076fc87ae49b4a1783
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-03-15 15:39:54 -0700

    Start

---
+++ src/components/SideBarData.js
@@ -0,0 +1,15 @@
+export const SideBarData = [
+    {
+        title: "About",
+        link: "/home"
+    },
+    {
+        title: "Experience",
+        link: "/home"
+    },
+    {
+        title: "Contact",
+        link: "/home"
+    }
+
+]
---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+//import "/Users/salamaboulhosn/Documents/web-projects/personal-portfolio-react/src/components/SideBar.css"
+import "../App.css"
+import {SideBarData} from "./SideBarData"
+
+function SideBar() {
+    return (
+        <nav className="SideBar">
+            <ul className ="SideBarList">
+            {SideBarData.map((val, key)=> {
+            return<li key = {key} 
+                className="row"
+                onClick={()=>
+                {window.location.pathname = val.link} }
+                >
+                <div>{val.title}</div>
+            </li>;
+            
+            })}
+            </ul>
+            </nav>
+        );
+}
+
+export default SideBar;
