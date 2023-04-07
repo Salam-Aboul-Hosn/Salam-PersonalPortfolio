ea12a6516d107532a28f5e4e2b87362be578d6e5
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-04-07 04:09:57 -0700

    Restart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import SideBar from '/Users/salamaboulhosn/Documents/web-projects/personal-portfolio-react/src/components/SideBar.js'
 import "./App.css"
 function App() {
 
   return (
     <div className="App"> 
-    <SideBar />
-    <h1 id="h">Hey, I'm Salam<br /> Web Developer</h1>
     </div>
   );
 }
