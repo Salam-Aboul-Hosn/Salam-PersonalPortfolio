725501d515cb4b602130b8b8f9cac5eaac293138
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-04-09 05:26:19 -0700

    Working on navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Navbarcss from "../styles/components/Navbar.module.css"
+
+function Navbar() {
+    return (
+    <header>
+            <h1>logo</h1>
+            <nav>
+                <a href= "/">About</a>
+                <a href= "/">Experience</a>
+                <a href= "/">Projects</a>
+                <a href= "/">Contact</a>
+            </nav>
+    </header>
+    );
+    
+}
+export default Navbar;
