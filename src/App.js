b3154b5b4a583400ea2a44a27e8d0028ec76cd56
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-06-03 17:04:40 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import { Scrollbars } from 'react-custom-scrollbars-2';
 function App() {
   return (
     <div className='App'>
-      {/*Try wrapping component with a div and give it height of the view port */}
       <div style={{ height: '100vh' }}>
         <Navbar />
         <Header />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,89 @@
-import Navbarcss from "../styles/components/Navbar.module.css"
-import icon from "../images/LogoBlack.jpeg"
-import {Link} from "react-scroll"
-
+import Navbarcss from '../styles/components/Navbar.module.css';
+import icon from '../images/LogoBlack.jpeg';
+import { Link } from 'react-scroll';
+import { useState } from 'react';
 function Navbar() {
-    const imgStyle = {
-        backgroundColor: "#181818",
-        cursor: "pointer"
-      };
+  const imgStyle = {
+    backgroundColor: '#181818',
+    cursor: 'pointer',
+  };
 
+  const handleLogoClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-      const handleLogoClick = () => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
-        });
-      }
-      
-    return (
-    <header className = {Navbarcss.header}>
-            <img onClick={handleLogoClick} src= {icon} alt= "Icon" className = {Navbarcss.logo} width="50px"/>
-            <nav className = {Navbarcss.navbar}>
-                <Link to="About" spy={true} smooth={true} offset={-100} duration={500} className = {Navbarcss.anchor} href= "#src/components/About.jsx">About</Link>
-                <Link to="Experience" spy={true} smooth={true} offset={-100} duration={500} className = {Navbarcss.anchor} href= "/">Experience</Link>
-                <Link to="Projects" spy={true} smooth={true} offset={-100} duration={500} className = {Navbarcss.anchor} href= "/">Projects</Link>
-                <Link to="Contact" spy={true} smooth={true} offset={0} duration={500} className = {Navbarcss.anchor} href= "/">Contact</Link>
-            </nav>
+  const [active, setActive] = useState('nav__menu');
+  const [icons, setIcons] = useState('nav__toggler');
+  const navToggle = () => {
+    if (active === 'nav__menu') {
+      setActive('nav__menu nav__active');
+    } else setActive('nav__menu');
+
+    // Icon Toggler
+    if (icons === 'nav__toggler') {
+      setIcons('nav__toggler toggle');
+    } else setIcons('nav__toggler');
+  };
+
+  return (
+    <header className={Navbarcss.header}>
+      <img
+        onClick={handleLogoClick}
+        src={icon}
+        alt='Icon'
+        className={Navbarcss.logo}
+        width='50px'
+      />
+      <nav className={Navbarcss.navbar}>
+        <Link
+          to='About'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className={Navbarcss.anchor}
+          href='#src/components/About.jsx'
+        >
+          About
+        </Link>
+        <Link
+          to='Experience'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className={Navbarcss.anchor}
+          href='/'
+        >
+          Experience
+        </Link>
+        <Link
+          to='Projects'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className={Navbarcss.anchor}
+          href='/'
+        >
+          Projects
+        </Link>
+        <Link
+          to='Contact'
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          className={Navbarcss.anchor}
+          href='/'
+        >
+          Contact
+        </Link>
+      </nav>
     </header>
-    );
-    
+  );
 }
 export default Navbar;
