90ef5089cdd615f160e0357303a9f80a06717225
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2024-08-20 17:33:17 -0700

    Experience update

--- src/data/ExperienceData.jsx
+++ src/data/ExperienceData.jsx
@@ -10,11 +10,12 @@ export function ExperienceSmartTwig() {
         awareness of current issues.
       </li>
       <li className={Experiencecss.jobDescription}>
-      Created reusable React components, enhancing design uniformity and accelerating development.
-            </li>
+        Created reusable React components, enhancing design uniformity and
+        accelerating development.
+      </li>
       <li className={Experiencecss.jobDescription}>
-        Built complex forms in order for users to create organizations, programs,
-        and projects.
+        Built complex forms in order for users to create organizations,
+        programs, and projects.
       </li>
       <li className={Experiencecss.jobDescription}>
         Implemented React Hook Form with Zod to collect, validate, and submit
@@ -30,16 +31,20 @@ export function ExperiencePending() {
       <h2 className={Experiencecss.jobNameHeader}>MyMicrojourney</h2>
       <p className={Experiencecss.jobDate}>Oct - Feb 2024</p>
       <li className={Experiencecss.jobDescription}>
-      Built a Rest API using Java Spring Boot and utilized MySQL to store user data.
+        Built a Rest API using Java Spring Boot and utilized MySQL to store user
+        data.
       </li>
       <li className={Experiencecss.jobDescription}>
-      Developed landing pages, components, and forms using React completing 100% of the front end.
+        Developed landing pages, components, and forms using React completing
+        100% of the front end.
       </li>
       <li className={Experiencecss.jobDescription}>
-      Utilized Spring Security to perform a one way hash for passwords, increasing data security.
+        Utilized Spring Security to perform a one way hash for passwords,
+        increasing data security.
       </li>
       <li className={Experiencecss.jobDescription}>
-      Used Spring JPA to define data models and created User & Email services for sign up functionality.
+        Defined data models using JPA annotations and created User & Email
+        services for sign up process.
       </li>
     </div>
   );
